f6e37bcd4be95e09871538daca3e5ac59c1bc2df
Thainã Viadroski <14934264+thainaviadroski@users.noreply.github.com>
2026-01-06 07:37:32 -0300

    criação do método limpar, refatoração do metedo add e toString

--- src/ListaEncadeada/ListaEncadeada.java
+++ src/ListaEncadeada/ListaEncadeada.java
@@ -3,14 +3,35 @@ package ListaEncadeada;
 public class ListaEncadeada<T> {
 
     private No<T> inicio;
+    private No<T> ultimo;
 
     private int tamanho = 0;
 
 
     public void add(T elemento) {
         No<T> celula = new No<>(elemento);
-        this.inicio = celula;
+        if (this.tamanho == 0) {
+            this.inicio = celula;
+        } else {
+            this.ultimo.setProximo(celula);
+        }
+        this.ultimo = celula;
         this.tamanho++;
+    }
+
+    public void limpar() {
+
+        for (No<T> atual = this.inicio; atual != null; ) {
+            No<T> proximo = atual.getProximo();
+            atual.setElemento(null);
+            atual.setProximo(null);
+            atual = proximo;
+        }
+
+        this.inicio = null;
+        this.ultimo = null;
+        this.tamanho = 0;
+
     }
 
     public int getTamanho() {
@@ -19,8 +40,23 @@ public class ListaEncadeada<T> {
 
     @Override
     public String toString() {
-        return "ListaEncadeada{" +
-                "inicio=" + inicio +
-                '}';
+        if (this.tamanho == 0) {
+            return "[]";
+        }
+        StringBuilder builder = new StringBuilder("[");
+
+        No<T> atual = this.inicio;
+
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            atual = atual.getProximo();
+            builder.append(atual.getElemento()).append(",");
+        }
+        builder.append(atual.getElemento()).append("]");
+
+//        while (atual.getProximo() != null) {
+//            atual = atual.getProximo();
+//            builder.append(atual.getElemento()).append(",");
+//        }
+        return builder.toString();
     }
 }
--- src/ListaEncadeada/teste/TesteListaEncadeada.java
+++ src/ListaEncadeada/teste/TesteListaEncadeada.java
@@ -7,7 +7,15 @@ public class TesteListaEncadeada {
     public static void main(String[] args) {
         ListaEncadeada<Integer> list = new ListaEncadeada<Integer>();
         list.add(1);
+        list.add(4);
+        list.add(18);
+        list.add(10);
+        list.add(2025);
 
+        System.out.println(list);
+        System.out.println("Tamanho: " + list.getTamanho());
+
+        list.limpar();
         System.out.println(list);
         System.out.println("Tamanho: " + list.getTamanho());
     }
